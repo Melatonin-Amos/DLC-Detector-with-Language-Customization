03b71c8a220b349a3b8037cf2e130a4090f37816
Richardlxr <2323805802@qq.com>
2025-10-23 15:10:53 +0800

    First commit

--- gui/main_window.py
+++ gui/main_window.py
@@ -10,3 +10,31 @@
 # - MainWindow: 主窗口类
 #
 # 开发优先级：⭐ (第10-11周完成)
+
+import tkinter as tk
+# 创建主窗口
+root = tk.Tk()
+root.title("主窗口 - 实时视频预览与检测")
+
+# 获取屏幕宽高
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# 设置窗口大小为屏幕的 50%
+width = int(screen_width * 0.5) 
+height = int(screen_height * 0.5)
+
+# 计算居中位置
+x = int((screen_width - width) / 2)
+y = int((screen_height - height) / 2)
+
+# 设置窗口大小和位置
+root.geometry(f"{width}x{height}+{x}+{y}")
+#锁定长宽比
+root.resizable(False, False)
+# 改一个可爱滴图标
+root.iconbitmap('kawaii_icon.ico')
+
+root.mainloop()
+
+
